fa923d91436acb5d2b580dfcd0adf33eb40764f2
yubachiri <k.i.moso9532@gmail.com>
2020-06-19 19:47:43 +0900

    そこそこ実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase, { db } from './firebaseInitializer';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+  const [uid, setUid] = useState('')
+
+  useEffect(() => {
+    firebase.auth().signInAnonymously()
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // ログイン中
+        console.log('そっち')
+        setUid(user.uid)
+      } else {
+        // ログアウト中
+        console.log('こっち')
+        return <p>ログイン中...</p>
+      }
+    })
+  }, [])
+
+  const addTodo = async () => {
+    const text = todo
+    const uid = await firebase.auth().currentUser.uid
+    db
+      .collection('todos')
+      .doc(uid)
+      .collection('todos')
+      .add({
+        content: text,
+        done: false
+      })
+
+    setTodo('')
+  }
+
+  const fetchTodo = async () => {
+    const uid = await firebase.auth().currentUser.uid
+    db
+      .collection('todos')
+      .doc(uid)
+      .collection('todos')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(function(doc) {
+          console.log(doc.id, " => ", doc.data());
+          setTodos([...todos, doc.data()])
+        });
+      })
+  }
+
+  const todoList = () => {
+    fetchTodo()
+    return todos.map(todo => {
+      console.log(todo.content)
+      return (
+        <li>{todo.content}</li>
+      )
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h2>TODO</h2>
+        {todo}
+        <input
+          onChange={event => {
+            const text = event.target.value
+            setTodo(text)
+          }}
+        />
+
+        <button
+          onClick={() => addTodo()}
         >
-          Learn React
-        </a>
+          追加する
+        </button>
+
+        <button
+          onClick={fetchTodo}
+        >取得テスト
+        </button>
+
+        <ul>
+          {todoList()}
+        </ul>
       </header>
     </div>
   );
